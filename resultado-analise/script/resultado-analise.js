12b52da70034d196265034e6b6e5987341f842a5
jv171bn <jv171bn@gmail.com>
2026-05-26 17:14:27 -0300

    aeeee

--- resultado-analise/script/resultado-analise.js
+++ resultado-analise/script/resultado-analise.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", () => {
 const byId = (id) => document.getElementById(id);
 const fmt = new Intl.NumberFormat("pt-BR");
 
-const LIMIT_KEY = "engaja_used_limit_v6";
-const LAST_PROFILE_KEY = "engaja_last_profile_v6";
-const OPTIMIZATION_STATE_KEY = "engaja_optimization_state_v6";
+const LIMIT_KEY = "engaja_used_limit_v7";
+const LAST_PROFILE_KEY = "engaja_last_profile_v7";
+const OPTIMIZATION_STATE_KEY = "engaja_optimization_state_v7";
 
 const checkAdmin = () => {
   const urlParams = new URLSearchParams(window.location.search);
